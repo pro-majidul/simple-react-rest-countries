aaf485795ac53a9f6e273a13d80cba9a03f75d30
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-22 11:33:13 +0600

    updating state and in a array and pass parameter

--- src/Component/Countries/Country.jsx
+++ src/Component/Countries/Country.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
 import './country.css'
-const Country = ({item}) => {
+const Country = ({item ,handelMarksButton}) => {
     const {name ,flags , population , area , ccn3}= item;
     const [visited , setVisited] =useState(false);
     const handelVisited = () =>{
         setVisited(!visited)
         
     }
+    
     return (
         <div className={`country ${visited ? 'visite' :'non-visite'}`}>
-            <h3>Country Name {name?.common}</h3>
+            <h3 style={{color: visited? 'purple' : 'black'}}>Country Name {name?.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Population : {population}</p>
             <p>Area : {area}</p>
             <p>Country Code : {ccn3}</p>
+            <button style={{
+                marginBottom :'10px'
+            }} onClick={()=>handelMarksButton(item)}>Marks As Read</button>
+            <br />
             <button onClick={handelVisited}>{visited? "Visited" : "Going"}</button>
             {
                 visited? " I have to visited" :"I want to visite"
--- src/Component/Countries/countries.jsx
+++ src/Component/Countries/countries.jsx
@@ -5,17 +5,33 @@ import '../Country.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visiteCountries, setVisiteCountries] = useState([])
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data => setCountries(data))
     }, [])
+
+    const handelMarksButton = countries =>{
+        const newVisiteCountries = [...visiteCountries , countries]
+        setVisiteCountries(newVisiteCountries)
+    }
     return (
         <div>
             <h1>Countries {countries.length}</h1>
+            <div style={{
+                textAlign:'center'
+            }}>
+                <h2>Visited Country list : {visiteCountries.length}</h2>
+                {
+                    visiteCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                }                
+            </div>
             <div className="country-field">
                 {
-                    countries.map(country => <Country item={country}
+                    countries.map(country => <Country 
+                        item={country}
+                        handelMarksButton ={handelMarksButton}
                         key={country.cca3}></Country>)
                 }
             </div>
