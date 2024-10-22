78022a92e6243a540c90f4bf9bde0fabd2caa3ce
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-22 09:22:56 +0600

    visited button added and toogle them text

--- src/Component/Countries/Country.jsx
+++ src/Component/Countries/Country.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './country.css'
 const Country = ({item}) => {
-    const {name ,flags}= item;
+    const {name ,flags , population , area , ccn3}= item;
+    const [visited , setVisited] =useState(false);
+    const handelVisited = () =>{
+        setVisited(!visited)
+        
+    }
     return (
         <div className='country'>
             <h3>Country Name {name?.common}</h3>
-        
             <img src={flags?.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
+            <p>Country Code : {ccn3}</p>
+            <button onClick={handelVisited}>{visited? "Visited" : "Going"}</button>
+            {
+                visited? " I have to visited" :"I want to visite"
+            }
         </div>
     );
 };
--- src/Component/Countries/countries.jsx
+++ src/Component/Countries/countries.jsx
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country";
+import '../Country.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
+            .then(res => res.json())
+            .then(data => setCountries(data))
     }, [])
     return (
         <div>
             <h1>Countries {countries.length}</h1>
-            {
-                countries.map(country => <Country item ={country}
-                key={country.cca3}></Country>)
-            }
-            
+            <div className="country-field">
+                {
+                    countries.map(country => <Country item={country}
+                        key={country.cca3}></Country>)
+                }
+            </div>
+
 
         </div>
     );
